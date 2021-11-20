fb3fbc78d463da0916bfbfd312a89fb0d395b001
Daniel Garcia <grciazd@gmail.com>
2021-11-20 13:04:16 -0700

    adding lesson3

--- js101_lesson3/practice_easy_1/question5.js
+++
@@ -1,8 +0,0 @@
-// Question 5
-// What will the following code output?
-
-console.log(false == '0'); // True
-console.log(false === '0'); // False
-
-/* In JavaScript there is a script equality operator '==='
-and a non scrict operator '==' */
--- js101_lesson3/practice_easy_1/question8.js
+++
@@ -1,9 +0,0 @@
-// Question 8
-// How can we add the family pet, "Dino", to the following array?
-
-let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
-
-flintstones.push('Dino');
-
-//We can use the .push method for arrays
-
